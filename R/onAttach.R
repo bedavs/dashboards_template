07de999498db5f3aca49771430a1991bfdf9360a
Beatriz Valcarcel <GryMaryson.Groneng@fhi.no>
2019-01-03 15:50:58 +0100

    add function

---
+++ R/onAttach.R
@@ -0,0 +1,7 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("PACKAGE: xxxx")
+  packageStartupMessage("Version 2019.01.01")
+  packageStartupMessage("Developed by Your Name")
+  packageStartupMessage("Department of Infectious Disease Epidemiology and Modelling")
+  packageStartupMessage("Norwegian Institute of Public Health")
+}
